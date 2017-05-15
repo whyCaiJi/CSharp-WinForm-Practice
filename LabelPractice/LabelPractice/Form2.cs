6fd5dbe158965ca207da642cdcc78fbc6a13d5a4
WUHEYANG\wuheyang0617 <wuheyang0617@gmail.com>
2017-05-15 23:27:15 +0800

    Modified the new form and make a login page. - Some practice on TextBox and Button

--- LabelPractice/LabelPractice/Form2.cs
+++ LabelPractice/LabelPractice/Form2.cs
@@ -16,5 +16,38 @@ namespace LabelPractice
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty || textBox2.Text == string.Empty)
+            {
+                MessageBox.Show("Should not contain any empty field.", "Warning");
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox1.Focus();
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Welcome to the system!");
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox1.Focus();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox1.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 oldForm = new Form1();
+            oldForm.Show();
+        }
     }
 }
