c41917630eb95cbe7b7856d470bfd998727666ce
WUHEYANG\wuheyang0617 <wuheyang0617@gmail.com>
2017-05-29 17:03:12 +0800

    ImageList and ToolBar practice:     - To make a tool bar with images as button icons, an image list needs to be set up first     - Link the image list to tool bar using the attribute ImageList of toolbar     - Add buttons and specify the indexes via toolbar button editor

---
+++ ImageListPractice/ImageListPractice/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ImageListPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.toolBar1 = new System.Windows.Forms.ToolBar();
+            this.toolBarButton1 = new System.Windows.Forms.ToolBarButton();
+            this.toolBarButton2 = new System.Windows.Forms.ToolBarButton();
+            this.toolBarButton3 = new System.Windows.Forms.ToolBarButton();
+            this.toolBarButton4 = new System.Windows.Forms.ToolBarButton();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "e1.png");
+            this.imageList1.Images.SetKeyName(1, "e7.png");
+            this.imageList1.Images.SetKeyName(2, "e14.png");
+            this.imageList1.Images.SetKeyName(3, "e21.png");
+            // 
+            // toolBar1
+            // 
+            this.toolBar1.Buttons.AddRange(new System.Windows.Forms.ToolBarButton[] {
+            this.toolBarButton1,
+            this.toolBarButton2,
+            this.toolBarButton3,
+            this.toolBarButton4});
+            this.toolBar1.ButtonSize = new System.Drawing.Size(32, 36);
+            this.toolBar1.DropDownArrows = true;
+            this.toolBar1.ImageList = this.imageList1;
+            this.toolBar1.Location = new System.Drawing.Point(0, 0);
+            this.toolBar1.Name = "toolBar1";
+            this.toolBar1.ShowToolTips = true;
+            this.toolBar1.Size = new System.Drawing.Size(284, 55);
+            this.toolBar1.TabIndex = 0;
+            // 
+            // toolBarButton1
+            // 
+            this.toolBarButton1.ImageIndex = 0;
+            this.toolBarButton1.Name = "toolBarButton1";
+            this.toolBarButton1.Text = "功能操作1";
+            // 
+            // toolBarButton2
+            // 
+            this.toolBarButton2.ImageIndex = 1;
+            this.toolBarButton2.Name = "toolBarButton2";
+            this.toolBarButton2.Text = "功能操作2";
+            // 
+            // toolBarButton3
+            // 
+            this.toolBarButton3.ImageIndex = 2;
+            this.toolBarButton3.Name = "toolBarButton3";
+            this.toolBarButton3.Text = "功能操作3";
+            // 
+            // toolBarButton4
+            // 
+            this.toolBarButton4.ImageIndex = 3;
+            this.toolBarButton4.Name = "toolBarButton4";
+            this.toolBarButton4.Text = "功能操作4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.toolBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ToolBar toolBar1;
+        private System.Windows.Forms.ToolBarButton toolBarButton1;
+        private System.Windows.Forms.ToolBarButton toolBarButton2;
+        private System.Windows.Forms.ToolBarButton toolBarButton3;
+        private System.Windows.Forms.ToolBarButton toolBarButton4;
+    }
+}
+
