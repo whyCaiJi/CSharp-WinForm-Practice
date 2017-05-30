8a04384da707c2d59e4a16d17331a00fe1a5b9c8
WUHEYANG\wuheyang0617 <wuheyang0617@gmail.com>
2017-05-30 16:19:36 +0800

    Timer practice:     - A moving earth from right to left controlled by two buttons

---
+++ TimerPractice/TimerPractice/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left -= 5;
+            if (pictureBox1.Right < 0)
+            {
+                pictureBox1.Left = Width;
+            }
+        }
+    }
+}
