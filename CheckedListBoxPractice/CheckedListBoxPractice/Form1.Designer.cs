6fe1034ec722e23cec41b2cc0bb77e3eeec4f091
WUHEYANG\wuheyang0617 <wuheyang0617@gmail.com>
2017-06-03 19:50:27 +0800

    A simple practice on CheckedListBox     - Items can be moved to the left or to the right using 4 buttons     - For each operation, a log is generated in the third CheckedListBox

---
+++ CheckedListBoxPractice/CheckedListBoxPractice/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CheckedListBoxPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox3 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(25, 24);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(170, 196);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Location = new System.Drawing.Point(313, 26);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(175, 196);
+            this.checkedListBox2.TabIndex = 1;
+            // 
+            // checkedListBox3
+            // 
+            this.checkedListBox3.FormattingEnabled = true;
+            this.checkedListBox3.Location = new System.Drawing.Point(25, 249);
+            this.checkedListBox3.Name = "checkedListBox3";
+            this.checkedListBox3.Size = new System.Drawing.Size(463, 132);
+            this.checkedListBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(221, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(221, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = ">>";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(221, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "<-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(221, 184);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "<<";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 393);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox3);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ CheckedListBoxPractice/CheckedListBoxPractice/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckedListBoxPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Add("Sunday");
+            checkedListBox1.Items.Add("Monday");
+            checkedListBox1.Items.Add("Tuesday");
+            checkedListBox1.Items.Add("Wednesday");
+            checkedListBox1.Items.Add("Thursday");
+            checkedListBox1.Items.Add("Friday");
+            checkedListBox1.Items.Add("Saturday");           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (object o in checkedListBox1.CheckedItems)
+            {
+                checkedListBox2.Items.Add(o);
+            }
+
+            for (int i=0; i<checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.CheckedItems.Contains(checkedListBox1.Items[i]))
+                {
+                    checkedListBox3.Items.Add(checkedListBox1.Items[i].ToString() + " is moved to the right.");
+                    checkedListBox1.Items.Remove(checkedListBox1.Items[i]);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (object o in checkedListBox1.Items)
+            {
+                checkedListBox2.Items.Add(o);
+            }
+
+            checkedListBox1.Items.Clear();
+            checkedListBox3.Items.Add("Left items are all moved to the right.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (object o in checkedListBox2.CheckedItems)
+            {
+                checkedListBox1.Items.Add(o);
+            }
+
+            for (int i=0; i<checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox2.CheckedItems.Contains(checkedListBox2.Items[i]))
+                {
+                    checkedListBox3.Items.Add(checkedListBox1.Items[i].ToString() + " is moved to the left");
+                    checkedListBox2.Items.Remove(checkedListBox2.Items[i]);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (object o in checkedListBox2.Items)
+            {
+                checkedListBox1.Items.Add(o);
+            }
+
+            checkedListBox2.Items.Clear();
+            checkedListBox3.Items.Add("Right items are all moved to the left.");
+        }
+    }
+}
