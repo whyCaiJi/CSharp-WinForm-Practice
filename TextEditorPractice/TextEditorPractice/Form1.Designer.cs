db059149246874d2c54ad5127dfb3f77db763fe5
WUHEYANG\wuheyang0617 <wuheyang0617@gmail.com>
2017-06-05 16:16:30 +0800

    A simple text editor to understand the usage of:     - FileStream     - OpenFileDialog     - StreamReader     - StreamWriter

---
+++ TextEditorPractice/TextEditorPractice/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace TextEditorPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(23, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(376, 353);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "写入文本";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(261, 324);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "打开文本文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "保存编辑文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 20);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(364, 298);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(23, 383);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(376, 52);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "命名文本文件";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(261, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "创建文本文件";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 456);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TextEditorPractice/TextEditorPractice/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TextEditorPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private static string directory_path = "d:\\";
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text.Length == 0)
+                {
+                    MessageBox.Show("文件名禁止为空！", "警告");
+                }
+                else
+                {
+                    directory_path = directory_path + textBox1.Text.Trim() + ".txt";
+                    StreamWriter sw = File.CreateText(directory_path);
+                    button2.Enabled = true;
+                    button3.Enabled = false;
+                    button1.Enabled = true;
+                    richTextBox1.Enabled = true;
+                    MessageBox.Show("文件成功建立。", "消息");
+                    sw.Close();
+                }
+            }
+            catch (Exception mm)
+            {
+                MessageBox.Show("磁盘操作错误，原因： "
+                    + Convert.ToString(mm), "警报");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Title = "打开文本文件";
+                open.FileName = "";
+                open.AddExtension = true;
+                open.CheckFileExists = true;
+                open.CheckPathExists = true;
+                open.Filter = "文本文件(*.txt)|*.txt";
+                open.ValidateNames = true;
+
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    StreamReader sr = new StreamReader(open.FileName,
+                        System.Text.Encoding.Default);
+                    this.richTextBox1.Text = sr.ReadToEnd();
+                    sr.Close();
+                }
+
+                MessageBox.Show("文件打开成功！", "消息");
+            }
+            catch (Exception mm)
+            {
+                MessageBox.Show("磁盘操作错误， 原因： " + Convert.ToString(mm),
+                    "警报");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream textfile = File.Open(directory_path, FileMode.OpenOrCreate,
+                    FileAccess.Write);
+                StreamWriter sw = new StreamWriter(textfile, Encoding.
+                    GetEncoding("GB2312"));
+                sw.Write(richTextBox1.Text.ToString());
+                MessageBox.Show("文件写成功。", "警报");
+                sw.Close();
+            }
+            catch (Exception mm)
+            {
+                MessageBox.Show("磁盘操作错误， 原因： " + Convert.ToString(mm),
+                    "警报");
+            }
+        }
+    }
+}
