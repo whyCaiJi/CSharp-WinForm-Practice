a1a37f5b22e882d4dd944c95383964d7594721f3
WUHEYANG\wuheyang0617 <wuheyang0617@gmail.com>
2017-06-05 14:07:56 +0800

    An example of how to use File class to create, copy and delete a file.

---
+++ FileCopyPractice/FileCopyPractice/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileCopyPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string somefile = @"D:\test.txt";
+            string target = @"D:\1.txt";
+            if (!File.Exists(somefile))
+            {
+                MessageBox.Show("File does not exist!");
+            }
+            else
+            {
+                if (File.Exists(target))
+                {
+                    File.Delete(target);
+                }
+                File.Copy(somefile, target);
+                MessageBox.Show("File successfully copied!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string target = @"D:\1.txt";
+            if (File.Exists(target))
+            {
+                File.Delete(target);
+            }
+            File.CreateText(target);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string target = @"D:\1.txt";
+            if (!File.Exists(target))
+            {
+                MessageBox.Show("File does not exist!");
+            }
+            else
+            {
+                File.Delete(target);
+                MessageBox.Show("File successfully deleted!");
+            }
+        }
+    }
+}
